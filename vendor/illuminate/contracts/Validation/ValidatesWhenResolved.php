7e60faa0d79020420bceb6a3a9dae1f9be326bde
GitHub Action <action@github.com>
2025-05-19 11:28:48 +0000

    release PHP 7.2 downgraded

---
+++ vendor/illuminate/contracts/Validation/ValidatesWhenResolved.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Jack202505\Illuminate\Contracts\Validation;
+
+interface ValidatesWhenResolved
+{
+    /**
+     * Validate the given class instance.
+     *
+     * @return void
+     */
+    public function validateResolved();
+}
