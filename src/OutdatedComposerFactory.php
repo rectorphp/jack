18fd33c2be4454a16e9a8e18a7f46b6ab9d3e8ce
GitHub Action <action@github.com>
2025-06-16 08:05:11 +0000

    release PHP 7.2 downgraded

--- src/OutdatedComposerFactory.php
+++ src/OutdatedComposerFactory.php
@@ -1,36 +1,35 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace Rector\Jack;
 
 use Rector\Jack\Mapper\OutdatedPackageMapper;
 use Rector\Jack\ValueObject\OutdatedComposer;
 use Rector\Jack\ValueObject\OutdatedPackage;
-
 /**
  * @see \Rector\Jack\Tests\OutdatedComposerFactory\OutdatedComposerFactoryTest
  */
-final readonly class OutdatedComposerFactory
+final class OutdatedComposerFactory
 {
-    public function __construct(
-        private OutdatedPackageMapper $outdatedPackageMapper
-    ) {
+    /**
+     * @readonly
+     * @var \Rector\Jack\Mapper\OutdatedPackageMapper
+     */
+    private $outdatedPackageMapper;
+    public function __construct(OutdatedPackageMapper $outdatedPackageMapper)
+    {
+        $this->outdatedPackageMapper = $outdatedPackageMapper;
     }
-
     /**
      * @param mixed[] $installedPackages
      */
-    public function createOutdatedComposer(array $installedPackages, string $composerJsonFilePath): OutdatedComposer
+    public function createOutdatedComposer(array $installedPackages, string $composerJsonFilePath) : OutdatedComposer
     {
         $outdatedPackages = $this->outdatedPackageMapper->mapToObjects($installedPackages, $composerJsonFilePath);
-
         // filter out dev packages, those are silently added, when "minimum-stability" is set to "dev"
-        $nonDevOutdatedPackages = array_filter(
-            $outdatedPackages,
-            fn (OutdatedPackage $outdatedPackage): bool => ! $outdatedPackage->lastestIsDevBranch()
-        );
-
+        $nonDevOutdatedPackages = \array_filter($outdatedPackages, function (OutdatedPackage $outdatedPackage) : bool {
+            return !$outdatedPackage->lastestIsDevBranch();
+        });
         return new OutdatedComposer($nonDevOutdatedPackages);
     }
 }
---
+++ vendor/illuminate/container/Attributes/CurrentUser.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Jack202506\Illuminate\Container\Attributes;
+
+use Attribute;
+#[Attribute(Attribute::TARGET_PARAMETER)]
+class CurrentUser extends Authenticated
+{
+    //
+}
