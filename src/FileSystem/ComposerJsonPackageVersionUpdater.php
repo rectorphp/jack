4b9810fad28535c9ad84b4e63601d677ecc9a162
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-06-27 20:24:06 +0700

    Fix

--- src/FileSystem/ComposerJsonPackageVersionUpdater.php
+++ src/FileSystem/ComposerJsonPackageVersionUpdater.php
@@ -11,12 +11,24 @@ final class ComposerJsonPackageVersionUpdater
     public static function update(string $composerJsonContents, string $packageName, string $newVersion): string
     {
         // replace using regex, to keep original composer.json format
-        return Strings::replace(
+        $allChanges = Strings::replace(
             $composerJsonContents,
             // find
             sprintf('#"%s": "(.*?)"#', $packageName),
             // replace
             sprintf('"%s": "%s"', $packageName, $newVersion)
         );
+
+        $suggestContent = Strings::match($composerJsonContents, '#"suggest"\s*:\s*{[^}]*}#');
+
+        if ($suggestContent !== null) {
+            $allChanges = Strings::replace(
+                $allChanges,
+                '#"suggest"\s*:\s*{[^}]*}#',
+                $suggestContent[0]
+            );
+        }
+
+        return $allChanges;
     }
 }
