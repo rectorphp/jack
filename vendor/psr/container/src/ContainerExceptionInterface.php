adf853eb00b374271671bee653ed5a5074430aa7
GitHub Action <action@github.com>
2025-08-19 08:46:01 +0000

    release PHP 7.2 downgraded

---
+++ vendor/psr/container/src/ContainerExceptionInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jack202508\Psr\Container;
+
+use Throwable;
+/**
+ * Base interface representing a generic exception in a container.
+ */
+interface ContainerExceptionInterface extends Throwable
+{
+}
