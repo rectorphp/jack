db291ec9ddebe825fba21f49c9cf49d41d2f8810
Tomas Votruba <tomas.vot@gmail.com>
2025-11-07 23:14:40 +0100

    exclude webmozart

--- scoper.php
+++ scoper.php
@@ -13,7 +13,7 @@ $timestamp = $nowDateTime->format('Ym');
 return [
     'prefix' => 'Jack' . $timestamp,
     'expose-constants' => ['#^SYMFONY\_[\p{L}_]+$#'],
-    'exclude-namespaces' => ['#^Rector\\\\Jack#', '#^Symfony\\\\Polyfill#', '#^PHPUnit\\\\'],
+    'exclude-namespaces' => ['#^Rector\\\\Jack#', '#^Webmozart\\\\#', '#^Symfony\\\\Polyfill#', '#^PHPUnit\\\\'],
     'exclude-files' => [
         // do not prefix "trigger_deprecation" from symfony - https://github.com/symfony/symfony/commit/0032b2a2893d3be592d4312b7b098fb9d71aca03
         // these paths are relative to this file location, so it should be in the root directory
