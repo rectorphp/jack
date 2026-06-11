3265f584aa21669536f34cc98f98ed557487c4a3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-06-12 03:26:25 +0700

    Handle version equal as not outdated (#43)
    * Handle version equal as not outdated
    
    * Fix

--- src/Composer/VersionComparator.php
+++ src/Composer/VersionComparator.php
@@ -14,4 +14,23 @@ final class VersionComparator
         // if major and minor are equal, we can skip the update
         return $firstMajor === $secondMajor && $firstMinor === $secondMinor;
     }
+
+    public static function areVersionsEqual(string $firstVersion, string $secondVersion): bool
+    {
+        return self::normalizeVersion($firstVersion) === self::normalizeVersion($secondVersion);
+    }
+
+    /**
+     * Make versions comparable, e.g. "v2.3" and "2.3.0" are the same version
+     */
+    private static function normalizeVersion(string $version): string
+    {
+        $version = ltrim($version, 'v');
+
+        while (preg_match('#^\d+(\.\d+)?$#', $version) === 1) {
+            $version .= '.0';
+        }
+
+        return $version;
+    }
 }
