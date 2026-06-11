9c075f5659d1dac5c541cdfeaa3aa705ef5e3de3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-06-12 03:03:38 +0700

    Fix

--- src/OutdatedComposerFactory.php
+++ src/OutdatedComposerFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Rector\Jack;
 
+use Rector\Jack\Composer\VersionComparator;
 use Rector\Jack\Mapper\OutdatedPackageMapper;
 use Rector\Jack\ValueObject\OutdatedComposer;
 use Rector\Jack\ValueObject\OutdatedPackage;
@@ -26,9 +27,14 @@ final readonly class OutdatedComposerFactory
         $outdatedPackages = $this->outdatedPackageMapper->mapToObjects($installedPackages, $composerJsonFilePath);
 
         // filter out dev packages, those are silently added, when "minimum-stability" is set to "dev"
+        // filter out false positives, where the latest version is the same as the current one
         $nonDevOutdatedPackages = array_filter(
             $outdatedPackages,
             fn (OutdatedPackage $outdatedPackage): bool => ! $outdatedPackage->lastestIsDevBranch()
+                && ! VersionComparator::areVersionsEqual(
+                    $outdatedPackage->getCurrentVersion(),
+                    $outdatedPackage->getLatestVersion()
+                )
         );
 
         return new OutdatedComposer($nonDevOutdatedPackages);
