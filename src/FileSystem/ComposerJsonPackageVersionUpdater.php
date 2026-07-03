22308a369c5022ad5d6b71159b8fbfa77e4a188a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-07-03 07:03:08 +0700

    Use JsonRecast for composer.json package version updates

--- src/FileSystem/ComposerJsonPackageVersionUpdater.php
+++ src/FileSystem/ComposerJsonPackageVersionUpdater.php
@@ -4,32 +4,46 @@ declare(strict_types=1);
 
 namespace Rector\Jack\FileSystem;
 
-use Nette\Utils\Strings;
+use Boundwize\JsonRecast\JsonRecast;
+use Boundwize\JsonRecast\Node\NodeJson;
+use Boundwize\JsonRecast\Node\StringNode;
+use Boundwize\JsonRecast\NodePath\NodeJsonPath;
+use Boundwize\JsonRecast\NodeVisitor\NodeJsonVisitorAbstract;
 
 final class ComposerJsonPackageVersionUpdater
 {
     public static function update(string $composerJsonContents, string $packageName, string $newVersion): string
     {
-        // replace using regex, to keep original composer.json format
-        $allChanges = Strings::replace(
-            $composerJsonContents,
-            // find
-            sprintf('#"%s": "(.*?)"#', $packageName),
-            // replace
-            sprintf('"%s": "%s"', $packageName, $newVersion)
+        $jsonDocument = JsonRecast::parse($composerJsonContents);
+
+        $jsonRecastResult = JsonRecast::traverse(
+            $jsonDocument,
+            new class($packageName, $newVersion) extends NodeJsonVisitorAbstract {
+                public function __construct(
+                    private readonly string $packageName,
+                    private readonly string $newVersion,
+                ) {
+                }
+
+                public function enterNode(NodeJson $nodeJson, NodeJsonPath $nodeJsonPath): ?NodeJson
+                {
+                    if (! $nodeJson instanceof StringNode) {
+                        return null;
+                    }
+
+                    foreach (['require', 'require-dev'] as $composerPackageSection) {
+                        if (! $nodeJsonPath->matches([$composerPackageSection, $this->packageName])) {
+                            continue;
+                        }
+
+                        return new StringNode($this->newVersion);
+                    }
+
+                    return null;
+                }
+            }
         );
 
-        $skippedKeys = ['suggest', 'replace', 'provide', 'conflict'];
-
-        foreach ($skippedKeys as $skippedKey) {
-            $regexKeyContent = sprintf('#"%s"\s*:\s*{[^}]*}#', $skippedKey);
-            $skippedContent = Strings::match($composerJsonContents, $regexKeyContent);
-
-            if ($skippedContent !== null) {
-                $allChanges = Strings::replace($allChanges, $regexKeyContent, $skippedContent[0]);
-            }
-        }
-
-        return $allChanges;
+        return JsonRecast::print($jsonRecastResult);
     }
 }
